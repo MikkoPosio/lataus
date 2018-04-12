fec6f2a59dca465386520f64fcad136e3ba739c5
Mikko Posio <mikko.posio@oamk.fi>
2018-04-12 16:16:36 +0300

    Add files via upload

---
+++ Tiedoston lataus_1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 12 13:48:31 2018
+
+@author: mikkopos
+"""
+import numpy as np
+import pandas as pd
+
+
+# Filepath
+#fp = r'C:\Users\mikkopos\Desktop\Python\Task\tTASKFILE.txt'
+#pandas.read_csv(C:\Users\mikkopos\Desktop\Python jutut\Task\TASKFILE.txt)
+with open('TASKFILE.txt', 'r') as f:
+    f_contents = f.read()
+    print(f_contents)
+    
+
+    
+    #f_contents = f.readlines()
+    #print(f_contents)
+    
+
+# Plot the data
+
